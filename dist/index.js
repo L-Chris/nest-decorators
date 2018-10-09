1f8bc0c3faf52072a7449a6d97098a3c5b6c7f8b
L-Chris <563303226@qq.com>
2018-10-09 23:50:14 +0800

    fix: cookie str must be a string

--- dist/index.js
+++ dist/index.js
@@ -2,4 +2,4 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const common_1 = require("@nestjs/common");
 const cookie_1 = require("cookie");
-exports.Cookie = common_1.createParamDecorator((data, req) => cookie_1.parse(req.headers.cookie, data));
+exports.Cookie = common_1.createParamDecorator((data, req) => cookie_1.parse(req.headers.cookie || '', data));
